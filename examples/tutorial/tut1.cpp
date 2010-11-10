bbe1b5c3b617b819b6ec0fafcbefb9142e620f6f
Karl Rupp <rupp@iue.tuwien.ac.at>
2010-11-10 19:38:45 +0100

    Initial commit, bare project skeleton is set up

---
+++ examples/tutorial/tut1.cpp
@@ -0,0 +1,23 @@
+/* =======================================================================
+   Copyright (c) 2010, Institute for Microelectronics, TU Vienna.
+   http://www.iue.tuwien.ac.at
+                             -----------------
+                     ViennaFEM - The Vienna Finite Element Module
+                             -----------------
+                            
+   authors:    Karl Rupp                          rupp@iue.tuwien.ac.at
+               Florian Rudolf                     flo.rudy+viennacl@gmail.com
+               Josef Weinbub                      weinbub@iue.tuwien.ac.at
+
+   license:    MIT (X11), see file LICENSE in the ViennaCL base directory
+
+   file changelog: - May 28, 2010   New from scratch for first release
+======================================================================= */
+
+// include necessary system headers
+#include <iostream>
+
+int main()
+{
+   std::cout << "Hello World!" << std::endl;
+}
---
+++ viennafem/forwards.h
@@ -0,0 +1,19 @@
+/* =======================================================================
+   Copyright (c) 2010, Institute for Microelectronics, TU Vienna.
+   http://www.iue.tuwien.ac.at
+                             -----------------
+                     ViennaFEM - The Vienna Finite Element Module
+                             -----------------
+                            
+   authors:    Karl Rupp                          rupp@iue.tuwien.ac.at
+               Florian Rudolf                     flo.rudy+viennacl@gmail.com
+               Josef Weinbub                      weinbub@iue.tuwien.ac.at
+
+   license:    MIT (X11), see file LICENSE in the ViennaCL base directory
+======================================================================= */
+
+/** @file forwards.h
+    @brief This file provides the forward declarations for the main types used within ViennaFEM
+*/
+
+
